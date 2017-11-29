56ec458fb58f5cccda2202431951ff0ffdf590a4
UttakarshTikku <utikku1993@gmail.com>
2017-11-29 12:10:55 +1100

    new html files included

--- CafeAppeSrc/startHere.js
+++ CafeAppeSrc/startHere.js
@@ -24,6 +24,22 @@ app.get('/viewCafe', function (req, res) {
     res.sendFile(constants.VIEW_CAFE_PATH);
 });
 
+app.get('/cafeAdmin', function (req, res) {
+    res.sendFile(constants.CAFE_ADMIN_PATH);
+});
+
+app.get('/addProduct', function (req, res) {
+    res.sendFile(constants.ADD_PRODUCT_PATH);
+});
+
+app.get('/viewProduct', function (req, res) {
+    res.sendFile(constants.VIEW_PRODUCT_PATH);
+});
+
+app.get('/archivedCafe', function (req, res) {
+    res.sendFile(constants.VIEW_ARCHIVED_CAFES);
+});
+
 var server = app.listen(5000, function () {
     console.log('Node server is running..');
 });
