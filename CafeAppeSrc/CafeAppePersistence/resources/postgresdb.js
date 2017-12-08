7a3cb0291ec1ed9a92d85fe43b173bf9c09c6910
UttakarshTikku <utikku1993@gmail.com>
2017-12-08 12:17:00 +1100

    Commiting data connection pool

--- CafeAppeSrc/CafeAppePersistence/resources/postgresdb.js
+++ CafeAppeSrc/CafeAppePersistence/resources/postgresdb.js
@@ -1,7 +1,4 @@
-// const pg = require('pg');
 var Pool = require('pg-pool');
-
-// var constants = require('../resources/constants');
 
 var connPool = new Pool({
   host: '127.0.0.1',
@@ -12,19 +9,5 @@ var connPool = new Pool({
   max: 20,
   min: 4
 });
-
-
-// const connectionString = process.env.DATABASE_URL || constants.SQL.DATASOURCE;
-
-// var dbConnection = new pg.Client({
-//   host: '127.0.0.1',
-//   port: 5432,
-//   database: 'postgres',
-//   user: 'dev',
-//   password: 'dev'
-// });
-// dbConnection.connect();
-
-// module.exports = dbConnection;
 
 module.exports = connPool;
